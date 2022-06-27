eef017f5f5fe0517dc1cdb873cf36c8ce4392936
kwright29 <khloewright329@gmail.com>
2022-06-27 10:28:47 -0700

    LoginVC implemented

---
+++ Parse Chat/LoginViewController.m
@@ -0,0 +1,126 @@
+//
+//  LoginViewController.m
+//  Parse Chat
+//
+//  Created by Khloe Wright on 6/27/22.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *usernameField;
+@property (strong, nonatomic) IBOutlet UITextField *passwordField;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)loginUser:(id)sender {
+    NSString *username = self.usernameField.text;
+        NSString *password = self.passwordField.text;
+        
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+            if (error != nil) {
+                NSLog(@"User log in failed: %@", error.localizedDescription);
+            } else {
+                NSLog(@"User logged in successfully");
+                
+                // display view controller that needs to shown after successful login
+            }
+        }];
+    
+    if ([username isEqual:@""] || [password isEqual:@""]) {
+        // TODO: create alert controller that alerts users when fields are empty
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Missing Field Required"
+                                                                                   message:@"One of your required fields is not complete. Please enter your information in the required field"
+                                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+        // create a cancel action
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                            style:UIAlertActionStyleCancel
+                                                          handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle cancel response here. Doing nothing will dismiss the view.
+                                                          }];
+        // add the cancel action to the alertController
+        [alert addAction:cancelAction];
+
+        // create an OK action
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle response here.
+                                                         }];
+        // add the OK action to the alert controller
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:^{
+            // optional code for what happens after the alert controller has finished presenting
+        }];
+    }
+}
+- (IBAction)registerUser:(id)sender {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameField.text;
+    //newUser.email = self.emailField.text;
+    newUser.password = self.passwordField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
+    
+    if ([self.usernameField.text isEqual:@""] || [self.passwordField.text isEqual:@""]) {
+        // TODO: create alert controller that alerts users when fields are empty
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Missing Field Required"
+                                                                                   message:@"One of your required fields is not complete. Please enter your information in the required field"
+                                                                            preferredStyle:(UIAlertControllerStyleAlert)];
+        // create a cancel action
+        UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel"
+                                                            style:UIAlertActionStyleCancel
+                                                          handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle cancel response here. Doing nothing will dismiss the view.
+                                                          }];
+        // add the cancel action to the alertController
+        [alert addAction:cancelAction];
+
+        // create an OK action
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                           style:UIAlertActionStyleDefault
+                                                         handler:^(UIAlertAction * _Nonnull action) {
+                                                                 // handle response here.
+                                                         }];
+        // add the OK action to the alert controller
+        [alert addAction:okAction];
+        
+        [self presentViewController:alert animated:YES completion:^{
+            // optional code for what happens after the alert controller has finished presenting
+        }];
+    }
+    
+
+}
+
+@end
